138a54a0beceaf226b535a80823a083fb41463dc
Nguyễn Văn Đức <83252725+NguyenDuck@users.noreply.github.com>
2022-01-30 20:13:42 +0700

    Update Log.java
    Change from android.util.Log to System.out & err

--- app/src/main/java/com/mithrilmania/blocktopograph/Log.java
+++ app/src/main/java/com/mithrilmania/blocktopograph/Log.java
@@ -18,22 +18,20 @@ public class Log {
 
     private static final String LOG_TAG = "Blocktopo";
 
-//    private static PrintWriter mFileLogger;
-
     private static String prependClassName(@NonNull Object caller, @NonNull String msg) {
         Class<?> clazz = caller instanceof Class ? (Class<?>) caller : caller.getClass();
         return clazz.getSimpleName() + ": " + msg;
     }
 
     public static void d(@NonNull Object caller, @NonNull String msg) {
-        android.util.Log.d(LOG_TAG, prependClassName(caller, msg));
+        System.out.println(prependClassName(caller, msg));
     }
 
     public static void d(@NonNull Object caller, @NonNull Throwable throwable) {
         StringWriter sw = new StringWriter(4096);
         PrintWriter pw = new PrintWriter(sw);
         throwable.printStackTrace(pw);
-        android.util.Log.e(LOG_TAG, prependClassName(caller, sw.toString()));
+        System.err.println(prependClassName(caller, sw.toString()));
         pw.close();
     }
 
